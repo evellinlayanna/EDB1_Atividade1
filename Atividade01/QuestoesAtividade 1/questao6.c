825cee031053d1ab1b2b2062a810773999dc5175
Evellin Layanna <72310367+evellinlayanna@users.noreply.github.com>
2024-10-27 11:45:17 -0300

    Update questao6.c

--- Atividade01/QuestoesAtividade 1/questao6.c
+++ Atividade01/QuestoesAtividade 1/questao6.c
@@ -1,46 +1,46 @@
-// // Lista de Exercıcios - Revisao de C para Estruturas de Dado
-// // Questao 6: Ponteiros e Alocacao Dinamica de Memoria
-// // Escreva um programa em C que aloque dinamicamente memoria para armazenar 5
-// // numeros inteiros, leia os numeros do usuario, calcule a soma e exiba o
-// // resultado. Nao se esqueca de liberar a memoria alocada ao final
+// Lista de Exercıcios - Revisao de C para Estruturas de Dado
+// Questao 6: Ponteiros e Alocacao Dinamica de Memoria
+// Escreva um programa em C que aloque dinamicamente memoria para armazenar 5
+// numeros inteiros, leia os numeros do usuario, calcule a soma e exiba o
+// resultado. Nao se esqueca de liberar a memoria alocada ao final
 
-// #include <stdio.h>
-// #include <stdlib.h> // Biblioteca necessaria para usar a funcao malloc que
-// aloca memoria dinamicamente
+#include <stdio.h>
+// Biblioteca necessaria para usar a funcao malloc que aloca memoria dinamicamente
+#include <stdlib.h>
 
-// int main() {
+int main() {
 
-//   // Declarando variaveis e ponteiros
-//   int *numeros;
-//   int soma = 0;
+  // Declarando variaveis e ponteiros
+  int *numeros;
+  int soma = 0;
 
-//   // Alocando memoria para armazenar 5 numeros inteiros
-//   numeros = (int *)malloc(5 * sizeof(int));
+  // Alocando memoria para armazenar 5 numeros inteiros
+  numeros = (int *)malloc(5 * sizeof(int));
 
-//   // Verifica se a alocacao de memoria foi bem sucedida
-//   if (numeros == NULL) {
-//     printf("Erro ao alocar memoria");
-//     return 1; // Retorna 1 para indicar erro
-//   }
+  // Verifica se a alocacao de memoria foi bem sucedida
+  if (numeros == NULL) {
+    printf("Erro ao alocar memoria");
+    return 1; // Retorna 1 para indicar erro
+  }
 
-//   // Loop para ler os 5 numeros do vetor
-//   printf("Insira 5 numeros inteiros:\n");
+  // Loop para ler os 5 numeros do vetor
+  printf("Insira 5 numeros inteiros:\n");
 
-//   for (int i = 0; i < 5; i++) {
-//     // Armazena o valor lido na posicao i do vetor
-//     scanf("%d", &numeros[i]);
-//   }
+  for (int i = 0; i < 5; i++) {
+    // Armazena o valor lido na posicao i do vetor
+    scanf("%d", &numeros[i]);
+  }
 
-//   for (int i = 0; i < 5; i++) {
-//     // Adiciona o valor lido ao ponteiro soma
-//     soma += numeros[i];
-//   }
+  for (int i = 0; i < 5; i++) {
+    // Adiciona o valor lido ao ponteiro soma
+    soma += numeros[i];
+  }
 
-//   // Exibe o resultado da soma
-//   printf("Soma dos numeros inseridos: %d\n", soma);
+  // Exibe o resultado da soma
+  printf("Soma dos numeros inseridos: %d\n", soma);
 
-//   // Libera a memoria alocada
-//   free(numeros);
+  // Libera a memoria alocada
+  free(numeros);
 
-//   return 0;
-// }
+  return 0;
+}
